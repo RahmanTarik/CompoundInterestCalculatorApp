54dc322624ce4083456a9f8f7ffd9fe718a30be5
Rahman Tarik <tarikseu@gamil.com>
2015-05-13 19:41:46 +0600

    Compound Interest

---
+++ CompoundInterestCalculatorApp/CompoundInterestCalculatorUI.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace CompoundInterestCalculatorApp
+{
+    partial class CompoundInterestCalculatorUi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.principalAmountTextBox = new System.Windows.Forms.TextBox();
+            this.interestRateTextBox = new System.Windows.Forms.TextBox();
+            this.compoundInterestTextBox = new System.Windows.Forms.TextBox();
+            this.yearsToGrowTextBox = new System.Windows.Forms.TextBox();
+            this.totalAmountTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // principalAmountTextBox
+            // 
+            this.principalAmountTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.principalAmountTextBox.Location = new System.Drawing.Point(134, 69);
+            this.principalAmountTextBox.Name = "principalAmountTextBox";
+            this.principalAmountTextBox.Size = new System.Drawing.Size(172, 20);
+            this.principalAmountTextBox.TabIndex = 0;
+            // 
+            // interestRateTextBox
+            // 
+            this.interestRateTextBox.Location = new System.Drawing.Point(134, 107);
+            this.interestRateTextBox.Name = "interestRateTextBox";
+            this.interestRateTextBox.Size = new System.Drawing.Size(89, 20);
+            this.interestRateTextBox.TabIndex = 1;
+            // 
+            // compoundInterestTextBox
+            // 
+            this.compoundInterestTextBox.Location = new System.Drawing.Point(134, 145);
+            this.compoundInterestTextBox.Name = "compoundInterestTextBox";
+            this.compoundInterestTextBox.Size = new System.Drawing.Size(100, 20);
+            this.compoundInterestTextBox.TabIndex = 2;
+            // 
+            // yearsToGrowTextBox
+            // 
+            this.yearsToGrowTextBox.Location = new System.Drawing.Point(134, 185);
+            this.yearsToGrowTextBox.Name = "yearsToGrowTextBox";
+            this.yearsToGrowTextBox.Size = new System.Drawing.Size(100, 20);
+            this.yearsToGrowTextBox.TabIndex = 3;
+            // 
+            // totalAmountTextBox
+            // 
+            this.totalAmountTextBox.Location = new System.Drawing.Point(134, 247);
+            this.totalAmountTextBox.Name = "totalAmountTextBox";
+            this.totalAmountTextBox.Size = new System.Drawing.Size(127, 20);
+            this.totalAmountTextBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Principal Amount";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Interest Rate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Compund Interest";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Years to Grow";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Future Total Amount";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(229, 110);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "%";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(253, 145);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "time(s) annually";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(256, 181);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(92, 23);
+            this.calculateButton.TabIndex = 4;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(57, 22);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(274, 24);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Compund Interest Calculator";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(312, 72);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(36, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Taka";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(270, 250);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(36, 13);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "Taka";
+            // 
+            // CompoundInterestCalculatorUi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(379, 370);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.totalAmountTextBox);
+            this.Controls.Add(this.yearsToGrowTextBox);
+            this.Controls.Add(this.compoundInterestTextBox);
+            this.Controls.Add(this.interestRateTextBox);
+            this.Controls.Add(this.principalAmountTextBox);
+            this.Name = "CompoundInterestCalculatorUi";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Iterest Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox principalAmountTextBox;
+        private System.Windows.Forms.TextBox interestRateTextBox;
+        private System.Windows.Forms.TextBox compoundInterestTextBox;
+        private System.Windows.Forms.TextBox yearsToGrowTextBox;
+        private System.Windows.Forms.TextBox totalAmountTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ CompoundInterestCalculatorApp/CompoundInterestCalculatorUI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompoundInterestCalculatorApp
+{
+    public partial class CompoundInterestCalculatorUi : Form
+    {
+        public CompoundInterestCalculatorUi()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            
+                double principalAmount = Convert.ToDouble(principalAmountTextBox.Text);
+                double interestRate = Convert.ToDouble(interestRateTextBox.Text) / 100;
+                int compoundInterest = Convert.ToInt32(compoundInterestTextBox.Text);
+                int yearsToGrow = Convert.ToInt32(yearsToGrowTextBox.Text);
+                double total = principalAmount * Math.Pow((1 + interestRate / compoundInterest), compoundInterest * yearsToGrow);
+                totalAmountTextBox.Text = Math.Round(total, 2).ToString();
+                ClearField();
+            
+        }
+
+        private void ClearField()
+        {
+            principalAmountTextBox.Text = String.Empty;
+            interestRateTextBox.Text = String.Empty;
+            compoundInterestTextBox.Text = String.Empty;
+            yearsToGrowTextBox.Text = String.Empty;
+        }
+
+       
+    }
+}
